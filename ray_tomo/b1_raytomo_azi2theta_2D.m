a4ad3376f78bbff05bda853a47b5624c04069294
jbrussell <jbrussell93@gmail.com>
2021-11-21 12:32:39 -0500

    small update

--- ray_tomo/b1_raytomo_azi2theta_2D.m
+++ ray_tomo/b1_raytomo_azi2theta_2D.m
@@ -406,9 +406,7 @@ for ip=1:length(Tperiods)
     if sum(size(GV)==size(Ac)) == 2
         GV_aznode = GV;
     else
-        [ymesh,xmesh] = meshgrid(ynode,xnode);
-        [ymesh_azi,xmesh_azi] = meshgrid(ynode_azi,xnode_azi);
-        GV_aznode = interp2(ymesh,xmesh,GV,ymesh_azi,xmesh_azi);
+        interp2(yi,xi,GV,yi_azi,xi_azi)
     end
     % get azimuthal anisotropy
     Ac2 = Ac.*GV_aznode; % s/km -> %
