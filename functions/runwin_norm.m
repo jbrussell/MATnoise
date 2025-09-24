8a71c23417e82ee1c50bb7962c5347359532e916
Josh Russell <jbrussell93@gmail.com>
2025-09-24 18:13:20 -0400

    ensure backwards compatibility with OBN

--- functions/runwin_norm.m
+++ functions/runwin_norm.m
@@ -4,4 +4,5 @@ function dataout = runwin_norm(datain)
 	dataout = datain(:)./smamp(:);
     Inan = find(isnan(dataout));
     dataout(Inan)=0;
+	dataout = dataout'; % transpose back to row vector
 return
